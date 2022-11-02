3c4327cf7fbc9524dce39d3286a3c890d9a38a6f
Alexey <alexey_beerman@mail.ru>
2022-11-02 03:42:24 +0300

    added selection sort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,23 @@
+﻿// Сортировка выбором.
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    System.Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Изначальный массив:  [" + string.Join(", ", array) + "]");
+for (int i = 0; i < n - 1; i++)
+{
+    int minPosition = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if(array[j] < array[minPosition]) minPosition = j;
+    }
+    int temporary = array[minPosition];
+    array[minPosition] = array[i];
+    array[i] = temporary;
+}
+Console.WriteLine("Отсортированный массив:  [" + string.Join(", ", array) + "]");
