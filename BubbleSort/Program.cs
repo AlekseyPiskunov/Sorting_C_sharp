7d9f8dffa91351615726d039d82b4c9834564ea0
Alexey <alexey_beerman@mail.ru>
2022-11-02 03:46:54 +0300

    added bubble sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пузырчатая сортировка.
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    System.Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Изначальный массив:  [" + string.Join(", ", array) + "]");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temporary = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temporary;
+        }
+    }
+    Console.WriteLine(i +  "[" + string.Join(", ", array) + "]");
+}
